6a1465a31a27b2c7e0e17e02f9ca7d8a53f6cc4f
Ariono IT <ariono.it@gmail.com>
2025-11-08 08:19:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6416182158-9398b",
+  "appId": "1:701546315359:web:60210275ec268e24c6e5ea",
+  "apiKey": "AIzaSyA3iGGq5RoHO6xirCwdKV2wNykVtOcaoPw",
+  "authDomain": "studio-6416182158-9398b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "701546315359"
+};
